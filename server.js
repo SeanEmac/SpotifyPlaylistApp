d5294bba600535126c8a3ec2ee952e6f8715da8b
seanemac <S.mccann11@nuigalway.ie>
2018-08-11 01:32:37 +0100

    Async and await working, callback hell solved

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const querystring = require('querystring');
-const rp = require('request-promise');
 const app = express();
+const axios = require('axios');
 require('dotenv').config();
 
 const redirect_uri = 'http://localhost:3000/callback'
@@ -21,127 +21,69 @@ app.get('/', function (req, res) {
   res.render('pages/index');
 })
 app.get('/home', function (req, res) {
-  res.render('pages/home', {
-    artistInfo: artistInfo
-  });
+  res.render('pages/home', { artistInfo: artistInfo });
 })
 
 app.post('/addToList', function(req, res){
   let artistName = req.body.artistName
-  let Options = {
-    url: `https://api.spotify.com/v1/search?q=${artistName}&type=artist&limit=1`,
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    json: true
-  }
-  request.get(Options, function (err, response, body) {
-    if(err){console.log(err.message)}
-    else{
-      console.log(body.artists.items[0])
-      let obj = {
-        img: body.artists.items[0].images[2].url,
-        name: body.artists.items[0].name,
-        artistId: body.artists.items[0].id
-      }
-      artistInfo.push(obj)
-      res.render('pages/home', {
-        artistInfo: artistInfo
-      });
+
+  await search(artistName).then(function(result) {
+    let artist = body.artist.items[0]
+    let obj = {
+      img: artist.url,
+      name: artist.name,
+      artistId: artist.id
     }
+    artistInfo.push(obj)
+    res.render('pages/home', { artistInfo: artistInfo });
   })
 })
 
-app.post('/getRecommendations', function (req, res) {
+app.post('/getRecommendations', async function (req, res) {
   let playlistLength = req.body.playlistLength
   let artistQuery = ""
 
   for(i=0; i < artistInfo.length; i++){
     artistQuery += artistInfo[i].artistId + ","
   }
-
+  
   artistQuery = artistQuery.slice(0, -1);
 
-  let Options = {
-    url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistQuery}&limit=${playlistLength}`,
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    json: true
-  }
-  request.get(Options, function (err, response, body) {
-    if(err){console.log(err.message)}
-    else{
-      let tracks = []
-      for(i = 0; i < body.tracks.length; i++){
-        let track = body.tracks[i]
-        let obj = {
-          number: i+1,
-          name: track.name,
-          artist: track.artists[0].name
-        }
-        tracks.push(obj)
-        trackURIs.push(track.uri)
+  await getRecommendations(artistQuery, playlistLength).then(function(result) {
+    let tracks = []
+    
+    for(i = 0; i < result.data.tracks.length; i++){
+      let track = result.data.tracks[i]
+      let obj = {
+        number: i+1,
+        name: track.name,
+        artist: track.artists[0].name
       }
-      res.render('pages/playlist', {
-        tracks: tracks
-      });
-    } 
-  });
+      tracks.push(obj)
+      trackURIs.push(track.uri)
+    }
+    res.render('pages/playlist', { tracks: tracks });
+  })
 
 })
 
-app.post('/makePlaylist', function (req, res) {
-  let playlistName = req.body.playlistName;
-  
-  let Options = {
-    url: 'https://api.spotify.com/v1/me',
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    json: true
-  }
-  request.get(Options, function (err, response, body) {
-    if(err){console.log(err.message)}
-    else{
-      let userId = body.id
+app.post('/makePlaylist', async function (req, res) {
+  let playlistName = req.body.playlistName
+  let userId = ""
+  let playlistId = ""
 
-      let Options = {
-        url: `https://api.spotify.com/v1/users/${userId}/playlists`,
-        headers: {
-          'Authorization': 'Bearer ' + access_token,
-          'Content-Type': 'application/json'
-        },
-        body: {
-          'name': playlistName
-        },
-        json: true
-      }
-      request.post(Options, function (err, response, body) {
-        if(err){console.log(err.message)}
-        else{
-          let playlistId = body.id
-          let Options = {
-            url: `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
-            headers: {
-              'Authorization': 'Bearer ' + access_token,
-              'Content-Type': 'application/json'
-            },
-            body: {
-              uris: trackURIs
-            },
-            json: true
-          }
-          request.post(Options, function (err, response, body) {
-            if(err){console.log(err.message)}
-            else{
-              res.render('pages/home', artistIds);
-            }
-          });
-        }
-      });
-    } 
-  });
+  await getUser().then(function(result) {
+    userId = result.data.id
+  })
+
+  await makePlaylist(userId, playlistName).then(function(result) {
+    playlistId = result.data.id
+  })
+
+  await addToPlaylist(userId, playlistId, trackURIs).then(function(result) {
+    res.render('pages/done')
+  })
+
 })
 
 // AUTHENTICATION
@@ -178,32 +120,70 @@ app.get('/callback', function(req, res) {
   })
 })
 
+//FUNCTIONS
+async function getRecommendations(artistQuery, playlistLength) {
+  return await axios({
+      url: `https://api.spotify.com/v1/recommendations?seed_artists=${artistQuery}&limit=${playlistLength}`,
+      method: 'get',
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      }
+  });
+}
+
+async function getUser() {
+  return await axios({
+      url: `https://api.spotify.com/v1/me`,
+      method: 'get',
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      }
+  });
+}
+
+async function makePlaylist(userId, playlistName) {
+  return await axios({
+      url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+      method: 'post',
+      headers: {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        'name': playlistName
+      }
+  });
+}
+
+async function addToPlaylist(userId, playlistId, trackURIs) {
+  return await axios({
+      url: `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+      method: 'post',
+      headers: {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json'
+      },
+      data: {
+        uris: trackURIs
+      }
+  });
+}
+
+async function search(artistName) {
+  return await axios({
+      url: `https://api.spotify.com/v1/search?q=${artistName}&type=artist&limit=1`,
+      method: 'get',
+      headers: {
+        'Authorization': 'Bearer ' + access_token
+      }
+  });
+}
+
 app.listen(3000, function () {
   console.log('Running on localhost:3000')
 })
 
 /*
-function getID(artistName){
-  let Options = {
-    url: `https://api.spotify.com/v1/search?q=${artistName}&type=artist&limit=1`,
-    headers: {
-      'Authorization': 'Bearer ' + access_token
-    },
-    json: true
-  }
-  rp(Options)
-    .then(function (response) {
-      return response.artists.items[0].id
-    })
-    .catch(function (err) {
-      console.log(err.message)
-    })
-}
-//let artist1 = "2o8lOQRjzsSC8UdbNN88HN"//mansions
-  //let artist2 = "2h93pZq0e7k5yf4dywlkpM"//frank Ocean
-  //let artist3 = "73sIBHcqh3Z3NyqHKZ7FOL"//Gambino
-  //let artist4 = "6l3HvQ5sa6mXTsMTB19rO5"//Jcole
-
   //let genre ="r-n-b"{
     //"genres" : [ "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music" ]
   //}
